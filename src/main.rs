78dd5a61dd6933b147bd0eeec8cc630408be9314
krolyxon <krolyxon@tutanota.com>
2023-07-13 21:33:02 +0530

    dialoguer

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use rust_fzf::{self, fzf_select};
+use dialoguer::theme::ColorfulTheme;
+use dialoguer::FuzzySelect;
 use std::fs;
 use std::io::{stdin, stdout, Write};
 use std::process::{Command, ExitStatus};
@@ -36,18 +37,25 @@ fn main() {
     combined_vec.extend(utils.into_iter());
 
     println!("Please select a language: ");
-    let choice = fzf_select(combined_vec);
-    if choice.is_empty() {
+    let choice = FuzzySelect::with_theme(&ColorfulTheme::default())
+        .with_prompt("Select a language/utility")
+        .default(0)
+        .items(&combined_vec[..])
+        .interact()
+        .unwrap();
+
+    if combined_vec.get(choice).is_none() {
+        // if combined_vec.get(choice) {
         println!("Please select a Language or a Utility");
         return;
     }
-    print!("Please enter a query for {}: ", &choice);
+    print!("Please enter a query for {}: ", &combined_vec[choice]);
     stdout().flush().unwrap();
     stdin()
         .read_line(&mut query)
         .expect("Error reading query from user");
 
-    let url = if languages.contains(&choice) {
+    let url = if languages.contains(&combined_vec[choice]) {
         query = query.replace(" ", "+").trim().parse().unwrap();
         format!("https://cht.sh/{}/{}", choice, query)
     } else {
