2649a2ec85c88cd94fee017a42aecde2a9fcf0c0
krolyxon <krolyxon@tutanota.com>
2023-07-20 22:49:43 +0530

    remove *.txt files and use Vec<String> instead

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use curl::easy::Easy;
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::FuzzySelect;
 use rust_fzf;
-use std::fs;
 use std::io::{stdin, stdout, Write};
 use std::process::Command;
 
@@ -16,26 +15,124 @@ const BANNER: &str = r#"
 "#;
 
 fn main() {
+    let languages: Vec<String> = vec![
+        String::from("arduino"),
+        String::from("ArnoldC"),
+        String::from("assembly/"),
+        String::from("awk"),
+        String::from("bash"),
+        String::from("basic"),
+        String::from("bf"),
+        String::from("c"),
+        String::from("chapel"),
+        String::from("clean"),
+        String::from("clojure"),
+        String::from("coffee"),
+        String::from("cpp"),
+        String::from("csharp"),
+        String::from("css"),
+        String::from("d"),
+        String::from("dart"),
+        String::from("delphi"),
+        String::from("dylan"),
+        String::from("eiffel"),
+        String::from("elisp"),
+        String::from("elixir"),
+        String::from("elm"),
+        String::from("erlang"),
+        String::from("factor"),
+        String::from("forth"),
+        String::from("fortran"),
+        String::from("fsharp"),
+        String::from("gdb"),
+        String::from("go"),
+        String::from("golang"),
+        String::from("groovy"),
+        String::from("haskell"),
+        String::from("html"),
+        String::from("java"),
+        String::from("javascript"),
+        String::from("js"),
+        String::from("julia"),
+        String::from("kotlin"),
+        String::from("latex"),
+        String::from("lisp"),
+        String::from("lua"),
+        String::from("matlab"),
+        String::from("nim"),
+        String::from("nodejs"),
+        String::from("objective-c"),
+        String::from("ocaml"),
+        String::from("octave"),
+        String::from("perl"),
+        String::from("perl6"),
+        String::from("php"),
+        String::from("pike"),
+        String::from("python"),
+        String::from("python3"),
+        String::from("r"),
+        String::from("racket"),
+        String::from("ruby"),
+        String::from("rust"),
+        String::from("scala"),
+        String::from("scheme"),
+        String::from("solidity"),
+        String::from("swift"),
+        String::from("tcl"),
+        String::from("tcsh"),
+        String::from("tmux"),
+        String::from("typescript"),
+        String::from("v"),
+        String::from("vb"),
+        String::from("vbnet"),
+        String::from("vlang"),
+        String::from("zsh"),
+    ];
+
+    let utils: Vec<String> = vec![
+        String::from("find"),
+        String::from("man"),
+        String::from("tldr"),
+        String::from("sed"),
+        String::from("awk"),
+        String::from("tr"),
+        String::from("cp"),
+        String::from("ls"),
+        String::from("grep"),
+        String::from("xargs"),
+        String::from("rg"),
+        String::from("ps"),
+        String::from("mv"),
+        String::from("kill"),
+        String::from("lsof"),
+        String::from("less"),
+        String::from("head"),
+        String::from("tail"),
+        String::from("tar"),
+        String::from("cp"),
+        String::from("rm"),
+        String::from("rename"),
+        String::from("jq"),
+        String::from("cat"),
+        String::from("ssh"),
+        String::from("cargo"),
+        String::from("git"),
+        String::from("git-worktree"),
+        String::from("git-status"),
+        String::from("git-commit"),
+        String::from("git-rebase"),
+        String::from("docker"),
+        String::from("docker-compose"),
+        String::from("stow"),
+        String::from("chmod"),
+        String::from("chown"),
+        String::from("make"),
+    ];
+
     let mut query = String::new();
-    let languagepath = "./src/languages.txt";
-    let utilpath = "./src/utils.txt";
     let mut easy = Easy::new();
 
     println!("{}", BANNER);
-
-    let languagedata = fs::read_to_string(languagepath).expect("Couldn't read languages");
-    let utildata = fs::read_to_string(utilpath).expect("Couldn't read utils");
-
-    let mut languages: Vec<String> = Vec::new();
-    let mut utils: Vec<String> = Vec::new();
-
-    for language in languagedata.split('\n') {
-        languages.push(language.to_string());
-    }
-    for util in utildata.split('\n') {
-        utils.push(util.to_string());
-    }
-
     let mut combined_vec: Vec<String> = languages.clone();
     combined_vec.extend(utils.into_iter());
 
